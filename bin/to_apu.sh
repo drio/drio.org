dc8dd0a6b519ae3414a3f8a501d9c37f18600137
David Rio Deiros <driodeiros@gmail.com>
2019-02-28 08:50:40 -0500

    minor changes

--- bin/to_apu.sh
+++ bin/to_apu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+DST="/usr/local/www/drio.org/public"
 rm -rf public/; hugo 
-ssh apu "rm -rf /usr/local/www/drio.org/public/*"
+ssh apu "mv $DST/v /tmp/vdrd; rm -rf $DST/*; mv /tmp/vdrd $DST/v"
 scp -r public/* apu:/usr/local/www/drio.org/public/
 
 rsync -avz -e ssh ../drio.org apu:src/
