179c0cfb741207c35596ee5c874b3a1b6fc69a33
David Rio Deiros <driodeiros@gmail.com>
2019-04-23 17:10:27 -0400

    updating gifs and bin

--- bin/to_apu.sh
+++ bin/to_apu.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+set -e
 
 DST="/usr/local/www/drio.org/public"
 rm -rf public/; hugo 
-ssh apu "mv $DST/v /tmp/vdrd; rm -rf $DST/*; mv /tmp/vdrd $DST/v"
-scp -r public/* apu:/usr/local/www/drio.org/public/
 
-rsync -avz -e ssh ../drio.org apu:src/
+ssh apu "mv $DST/v /tmp/vdrd; rm -rf $DST/*; mv /tmp/vdrd $DST/v"
+
+cd public
+rm -f /tmp/public.tar.gz
+tar zcf /tmp/public.tar.gz *
+cd ..
+scp /tmp/public.tar.gz apu:/tmp
+ssh apu "(cd $DST/; tar zxf /tmp/public.tar.gz; rm -f /tmp/public.tar.gz)"
+
+#scp -r public/* apu:/usr/local/www/drio.org/public/
+#rsync -avz -e ssh ../drio.org apu:src/
